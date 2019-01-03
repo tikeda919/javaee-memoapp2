87a75127856aceb69f10faacca7493e4f79b2d64
zaki <zaki.hmkc@gmail.com>
2019-01-04 08:13:50 +0900

    update: Serializableの実装

--- src/main/java/jp/example/www/MemoBean.java
+++ src/main/java/jp/example/www/MemoBean.java
@@ -1,6 +1,9 @@
 package jp.example.www;
 
-public class MemoBean {
+import java.io.Serializable;
+
+public class MemoBean implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     public MemoBean() {
     }
